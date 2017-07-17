4338a4cd64f030b27b7fef9ec415a396933c2b69
Devon Barrett <devon@devonbarrett.net>
2017-07-17 07:38:41 +0100

    adds keycode regex

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ module.exports =
 {
   hooks: {
     "page": function(page) {
-      // modifies page content
-      console.log('ding!', page);
-      // collectStore(page, this, true);
+      page.content += `<script>
+  var bookBody = document.body.getElementsByClassName('book-body')[0];
+    if(bookBody) {
+    bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">shift\+/g,'<span class="keycode">&#x21E7;</span>+<span class="keycode">');
+    if (navigator.platform === 'MacIntel') {
+      bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">ctrl\+/g,'<span class="keycode">&#x2318;</span>+<span class="keycode">');
+    } else {
+      bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">ctrl\+/g,'<span class="keycode">ctrl</span>+<span class="keycode">');
+    }
+  }
+</script>`;
       return page;
     }
   }
