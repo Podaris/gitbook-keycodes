de6380b6613e4c2bc81f61f20070487745aacedd
Nathan Koren <nkoren@gmail.com>
2017-10-01 22:35:04 +0100

    Parse a naked 'shift' corrrectly

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ module.exports =
     "page": function(page) {
       page.content += `<script>
   var bookBody = document.body.getElementsByClassName('book-body')[0];
-    if(bookBody) {
-    bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">shift\+/g,'<span class="keycode">&#x21E7;</span>+<span class="keycode">');
+  if (bookBody) {
+    bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">shift<\\/span>/g,'<span class="keycode">&#x21E7; SHIFT</span>');
+    bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">shift\\+/g,'<span class="keycode">&#x21E7;</span>+<span class="keycode">');
     if (navigator.platform === 'MacIntel') {
-      bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">ctrl\+/g,'<span class="keycode">&#x2318;</span>+<span class="keycode">');
+      bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">ctrl\\+/g,'<span class="keycode">&#x2318;</span>+<span class="keycode">');
     } else {
-      bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">ctrl\+/g,'<span class="keycode">ctrl</span>+<span class="keycode">');
+      bookBody.innerHTML = bookBody.innerHTML.replace(/<span class="keycode">ctrl\\+/g,'<span class="keycode">ctrl</span>+<span class="keycode">');
     }
   }
 </script>`;
